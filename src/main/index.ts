3d31754f10a1d3b8e2cb8e4554e837e2e6379a9a
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-02-18 08:48:18 +0800

    fix(hashedFileName): crypto js doesnt support built in bcrypt, additional package required to install, instead bring back the encryption to md5

--- src/main/index.ts
+++ src/main/index.ts
@@ -110,7 +110,7 @@ const isFileStable = async ({
 
 const hashedFileName = async (filePath: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const hash = crypto.createHash('bcrypt')
+    const hash = crypto.createHash('md5')
     const stream = fs.createReadStream(filePath)
     stream.on('data', (data) => hash.update(data))
     stream.on('end', () => resolve(hash.digest('hex')))
