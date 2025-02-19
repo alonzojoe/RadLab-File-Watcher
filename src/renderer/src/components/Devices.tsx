449473e290bd41ad69030c474c370d7ab1421fd4
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-02-19 09:11:03 +0800

    feat: add network device checker use custom hook useEventListener to check wether the device is offline/online with dynamic style and text.

--- src/renderer/src/components/Devices.tsx
+++ src/renderer/src/components/Devices.tsx
@@ -1,14 +1,40 @@
+import { useState, useEffect } from 'react'
 import { FaNetworkWired } from 'react-icons/fa6'
 import { BsFillDeviceHddFill } from 'react-icons/bs'
+import useEventListener from '@renderer/hooks/useEventListener'
 
 const Devices = (): JSX.Element => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine)
+
+  const handleOnline = (): void => setIsOnline(true)
+  const handleOffline = (): void => setIsOnline(false)
+
+  useEventListener({
+    eventName: 'online',
+    handler: handleOnline,
+    element: window
+  })
+
+  useEventListener({
+    eventName: 'offline',
+    handler: handleOffline,
+    element: window
+  })
+
+  const style = {
+    network: {
+      class: isOnline ? `text-green-400` : `text-red-400`,
+      text: isOnline ? 'Connected' : 'Disconnected'
+    }
+  }
+
   return (
     <div className="flex items-center justify-center">
       <div className="border border-primaryBg border-r-textSecondary pr-10 flex gap-3">
         <FaNetworkWired className="text-primary text-3xl" />
         <div>
           <h4 className="font-semibold">Network</h4>
-          <span className="text-green-400 text-sm">Connected</span>
+          <span className={`${style.network.class} text-sm`}>{style.network.text}</span>
         </div>
       </div>
       <div className="border border-primaryBg border-l-textSecondary pl-10 flex gap-3">
