a29b36f8dae8c94022a3e004cc976abceb54a579
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-03 09:31:51 +0800

    config(db cred): update database configruation to development mode

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,5 +2,5 @@ import type { MappedDrives } from '../types/types'
 
 export const DIRECTORIES: MappedDrives = {
   orders_directory: 'E:\\ORDERS_TEST',
-  target_directory: 'E:\\TARGET_TEST'
+  target_directory: 'E:\\TARGET_TEST\\LIS'
 }
